d40651ea0ddf203ef921db71db3a9f3ba5ed2d88
Karan Sharma <nikhilsharma7355796130@gmail.com>
2024-01-06 20:07:57 +0545

    updated script

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 
         function genQR()
         {
-            var gapi="https://chart.googleapis.com/chart?chf=bg,s,65432100&cht=qr&chs=";
-            var myimg=document.getElementById("img");
-            var mytext=document.getElementById("qrtext").value;
-            var mysize=document.getElementById("size").value;
+            let gapi="https://api.qrserver.com/v1/create-qr-code/?data=";
+            let myimg=document.getElementById("img");
+            let mytext=document.getElementById("qrtext").value;
+            let mysize=document.getElementById("size").value;
 
             if (mytext!=="" && mysize=="100")
             {
-                myimg.src=gapi+"100x100"+"&chl="+mytext;
-                // https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl=hello
-            }
+                myimg.src=gapi+mytext+"!"+";"+"&size=100x100";
+                //http://api.qrserver.com/v1/create-qr-code/?data=HelloWorld!&size=100x100
 
-            else if(mytext!=="" && mysize=="150")
+              }  else if(mytext!=="" && mysize=="150")
             {
 
-                myimg.src=gapi+"150x150"+"&chl="+mytext;
+                myimg.src=gapi+mytext+"!"+";"+"&size=150x150";
             }
 
             else if(mytext!=="" && mysize=="200")
             {
-                myimg.src=gapi+"200x200"+"&chl="+mytext;
+                myimg.src=gapi+mytext+"!"+";"+"&size=200x200";
             }
 
             else if (mytext!=="" && mysize=="250")
             {
-                myimg.src=gapi+"250x250"+"&chl="+mytext;
+                myimg.src=gapi+mytext+"!"+";"+"&size=250x250";
             }
 
             else if (mytext!=="" && mysize=="300")
             {
-                myimg.src=gapi+"300x300"+"&chl="+mytext;
+                myimg.src=gapi+mytext+"!"+";"+"&size=300x300";
             }
 
             else
